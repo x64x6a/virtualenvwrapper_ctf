453a283fe18aea5d14f5acea4f2ead9545393341
x64x6a <x64x6a@users.noreply.github.com>
2021-04-16 01:53:45 -0700

    Added generic classes for CTFd support

---
+++ vctf/ctf/__init__.py
@@ -0,0 +1,28 @@
+
+
+class CTF(object):
+    def __init__(self, platform, name, username, password):
+        pass
+    def pull(self):
+        """
+        pull challenges
+        """
+        pass
+    def list(self):
+        """
+        list challenges
+        """
+        pass
+    def add(self):
+        """
+        manually add challenge
+        """
+        pass
+    def delete(self):
+        """
+        manually delete challenge
+        """
+        pass
+
+class CTFd(CTF):
+    pass
