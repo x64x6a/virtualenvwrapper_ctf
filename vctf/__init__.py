0acfa984e16d7584e59d6dadfcd4c290081257c1
x64x6a <x64x6a@users.noreply.github.com>
2021-04-16 01:55:44 -0700

    Moved ctf.py code to __init__.py

--- vctf/__init__.py
+++ vctf/__init__.py
@@ -1,5 +1,18 @@
 """
 virtualenvwrapper_ctf
 """
+import logging
+import os
+import subprocess
+
 
 __import__('pkg_resources').declare_namespace(__name__)
+
+log = logging.getLogger(__name__)
+
+def template(args):
+    project_name = os.path.basename(os.getenv('VIRTUAL_ENV'))
+    project_path = os.getenv('PROJECT_HOME')
+
+    filepath = os.path.join(project_path, project_name)
+    subprocess.check_call(['initctf', filepath])
