50517cd5e2cf88a9afe9e614dc2a8ff635f766c2
x64x6a <x64x6a@users.noreply.github.com>
2021-04-25 19:46:36 -0700

    Added CTFd login and challenge support

--- vctf/__main__.py
+++ vctf/__main__.py
@@ -9,9 +9,10 @@ import os
 import subprocess
 import sys
 
-from vctf.vctf import init, get_ctf_config
-from vctf.vctf import set_active
+from vctf.vctf import init, add_challenge
+from vctf.vctf import get_ctf_config, set_active, get_active
 
+from vctf.ctf import get_plaforms, get_platform_object
 
 log = logging.getLogger(__name__)
 
@@ -20,30 +21,119 @@ def main():
         prog=(__package__ or __file__),
         description='Copy files into CTF work directory',
     )
-    parser.add_argument('--add', '-a', dest='add', nargs=2, metavar=('category', 'challenge'),
-        help='Add a challenge to the active CTF')
-    parser.add_argument('--init', '-i', dest='init', metavar='ctf_name',
-        help='Initialize CTF within $PROJECT_HOME directory with given name')
-    parser.add_argument('--start', '-s', dest='start', metavar='ctf_name',
-        help='Start and initialize CTF within $PROJECT_HOME directory with given name')
-    parser.add_argument('--end', '-e', dest='end', action='store_true',
-        help='End active CTF')
+    # base arguments
+    parser.add_argument('command',
+        help='Specify a command to run: init, start, end, add, list, pull, submit')
+    parser.add_argument('args', nargs='*',
+        help='[name] or [category challenge] or [challenge_id flag]')
+
+    # customized arguments, can also set in .ini files
+    parser.add_argument('--username', '-u')
+    parser.add_argument('--password', '-p')
+    parser.add_argument('--url', '-d', help='Root web URL')
+    parser.add_argument('--platform', '-f', help='Hosting platform, supported: ' + ','.join(get_plaforms()))
+    parser.add_argument('--directory', help='Specify CTF directory')
+    parser.add_argument('--project_home', help='Specify directory if $PROJECT_HOME not used')
 
     args = parser.parse_args()
+    command = args.command
 
-    if args.add:
-        from .addchallenge import add_challenge
-        category, name = args.add
-        add_challenge(category, name)
-    elif args.init:
-        name = args.init
+    #init(config, name, username=None, password=None, url=None, platform=None, directory=None, project_home=None)
+    if command == 'init' or command == 'i':
+        if args.args is None or len(args.args) == 0:
+            parser.error("the following arguments are required: args.  [name] not given")
+        name = args.args[0]
+        init_kwargs = {
+            "username": args.username, "password": args.password,
+            "url": args.url, "platform": args.platform,
+            "directory": args.directory, "project_home": args.project_home
+        }
         config = get_ctf_config(name)
-        init(config, name)
-    elif args.start:
-        name = args.start
-        set_active(name)
-    elif args.end:
+        init(config, name, **init_kwargs)
+    elif command == 'start' or command == 's':
+        if args.args is None or len(args.args) == 0:
+            parser.error("the following arguments are required: args.  [name] not given")
+        name = args.args[0]
+        set_active(args.name)
+    elif command == 'end' or command == 'e':
         subprocess.check_call(['endctf'])
+    elif command == 'add' or command == 'a':
+        if args.args is None or len(args.args) < 2:
+            parser.error("the following arguments are required: args.  [category challenge] not given")
+        category = args.args[0]
+        challenge = args.args[1]
+        add_challenge(category, challenge)
+    elif command == 'list' or command == 'l':
+        if args.args is None or len(args.args) == 0:
+            try:
+                name = get_active()
+            except FileNotFoundError:
+                name = None
+            if name is None or len(name) == 0:
+                parser.error("the following arguments are required: args.  [name] not given")
+        else:
+            name = args.args[0]
+        config = get_ctf_config(name)
+        c = get_platform_object(config)
+        c.list()
+    elif command == 'pull' or command == 'p':
+        if args.args is None or len(args.args) == 0:
+            try:
+                name = get_active()
+            except FileNotFoundError:
+                name = None
+            if name is None or len(name) == 0:
+                parser.error("the following arguments are required: args.  [name] not given")
+        else:
+            name = args.args[0]
+        config = get_ctf_config(name)
+        c = get_platform_object(config)
+        c.pull()
+    elif command == 'submit':
+        try:
+            name = get_active()
+        except FileNotFoundError:
+            parser.error("must be in an active CTF")
+        if args.args is None or len(args.args) < 2:
+            parser.error("the following arguments are required: args.  [challenge_id flag] not given")
+        id = args.args[0]
+        flag = args.args[1]
+        config = get_ctf_config(name)
+        c = get_platform_object(config)
+        c.submit(id, flag)
+    else:
+        parser.error("command not supported")
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+"""
--- vctf/ctf/__init__.py
+++ vctf/ctf/__init__.py
@@ -1,8 +1,31 @@
+import configparser
+import json
+import os
+import re
+import string
+
+import pandas
+import requests
+from urllib.parse import urlparse
+
+
+# for parsing challenge names, characteres chosen for personal preferance
+parsed_challenge_alphabet = string.ascii_letters + string.digits  + '-_'
 
 
 class CTF(object):
-    def __init__(self, platform, name, username, password):
-        pass
+    def __init__(self, config_filename):
+        config_parser = configparser.ConfigParser()
+        config_parser.read(config_filename)
+
+        #  this will key error if fields do not exist
+        self.name = config_parser['DEFAULT']['name']
+        self.username = config_parser['DEFAULT']['username']
+        self.password = config_parser['DEFAULT']['password']
+        self.platform = config_parser['DEFAULT']['platform']
+        self.url = config_parser['DEFAULT']['url']
+        self.directory = config_parser['DEFAULT']['directory']
+        self.project_home = config_parser['DEFAULT']['project_home']
     def pull(self):
         """
         pull challenges
@@ -23,6 +46,194 @@ class CTF(object):
         manually delete challenge
         """
         pass
+    def submit(self, id, flag):
+        """
+        submit a flag for a challenge
+        """
+        pass
 
 class CTFd(CTF):
-    pass
+    def __init__(self, config_filename):
+        super().__init__(config_filename)
+        self._auth()
+
+    def _auth(self):
+        """
+        Sets session using username/password
+        """
+        self.session = requests.session()
+
+        # get nonces/tokens
+        r = self.session.get(self.url + '/login')
+        if not r.ok:
+            raise Exception("Error querying site")
+
+        # get nonce
+        regex = re.compile('<input id="nonce" name="nonce" type="hidden" value="(.+)">')
+        found = regex.findall(r.text)
+        if len(found) < 1:
+            raise Exception("nonce not found")
+        nonce = found[0]
+
+        # get csrfNonce
+        regex = re.compile("""'csrfNonce': "(.+)",""")
+        found = regex.findall(r.text)
+        if len(found) < 1:
+            raise Exception("csrfNonce not found")
+        self.csrfNonce = found[0]
+
+        # login with creds to set cookie
+        auth_args = {
+            "name": self.username,
+            "password": self.password,
+            "nonce": nonce,
+        }
+        r = self.session.post(self.url + '/login', data=auth_args)
+        if not r.ok:
+            raise Exception("Error logging in")
+
+    def _challenges(self):
+        """
+        Requests challenges from CTFd api
+        """
+        route = '/api/v1/challenges'
+        r = self.session.get(self.url + route)
+        if not r.ok:
+            print(r)
+            print(r.text)
+            raise Exception("Error getting challenges")
+
+        results = json.loads(r.text)
+        challenges = results["data"]
+        return challenges
+
+    def _challenge(self, id):
+        """
+        Requests a challenge from CTFd api
+        """
+        route = '/api/v1/challenges/' + str(id)
+        r = self.session.get(self.url + route)
+        if not r.ok:
+            print(r)
+            print(r.text)
+            raise Exception("Error getting challenges")
+
+        results = json.loads(r.text)
+        challenge = results["data"]
+        return challenge
+
+    def _attempt(self, id, flag):
+        """
+        Attempt a flag for a given challenge
+        """
+        # update csrf token
+        route = '/challenges'
+        r = self.session.get(self.url + route)
+        if not r.ok:
+            raise Exception("Error querying site")
+
+        # get csrfNonce
+        regex = re.compile("""'csrfNonce': "(.+)",""")
+        found = regex.findall(r.text)
+        if len(found) < 1:
+            raise Exception("csrfNonce not found")
+        self.csrfNonce = found[0]
+
+
+        route = '/api/v1/challenges/attempt'
+        data = {"challenge_id": int(id), "submission": flag}
+        headers = {"CSRF-Token": self.csrfNonce}
+        r = self.session.post(self.url + route, json=data, headers=headers)
+        if not r.ok:
+            raise Exception("Error getting challenges")
+
+        results = json.loads(r.text)
+        status = results["data"]
+        return status
+
+    def list(self):
+        """
+        Prints challenges to stdout
+        """
+        challenges = self._challenges()
+        columns = ["id", "category", "name", "value", "solves", "solved_by_me"]
+        df = pandas.DataFrame(challenges, columns=columns)
+        print(df)
+
+    def pull(self):
+        """
+        Gets challenges and makes appropriate directories locally
+        """
+        from vctf.vctf import add_challenge
+
+        challenges = self._challenges()
+        for c in challenges:
+            id = c['id']
+            challenge = self._challenge(id)
+
+            name = challenge["name"]
+            category = challenge["category"]
+            files = challenge["files"]
+            #view = challenge["view"]
+            #value = challenge["value"]
+            #description = challenge["description"]
+
+            name = parse_challenge_name(name)
+            category = parse_challenge_name(category)
+            challenge_path = add_challenge(category, name)
+            s = "Challenge: {cat} - {name} ({id}): {path}".format(cat=category, name=name, id=id, path=challenge_path)
+            print(s)
+
+            for route in files:
+                r = urlparse(route)
+                filename = os.path.basename(r.path)
+
+                # quick check to determine if we escape up a directory
+                challenge_file = os.path.join(challenge_path, filename)
+                c = os.path.realpath(challenge_file)
+                d = os.path.realpath(challenge_path)
+                if d != os.path.commonpath((c,d)):
+                    print("error adding file for challenge {id}".format(id=id))
+                    continue
+                if not os.path.exists(c):
+                    url = self.url + route
+                    r = self.session.get(url)
+                    with open(c, 'wb') as f:
+                        f.write(r.content)
+                    print(" [+] Added file for challenge {id} '{filename}'".format(id=id, filename=c))
+
+    def submit(self, id, flag):
+        """
+        Submits a flag for a challenge
+        """
+        status = self._attempt(id, flag)
+        message = status['message']
+        print(message)
+        if status == 'incorrect':
+            return False
+        else:
+            return True
+
+
+platforms = {
+    "CTFd": CTFd,
+}
+
+def get_plaforms():
+    return [k for k in platforms]
+
+def get_platform_object(config_filename):
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_filename)
+    platform = config_parser['DEFAULT']['platform']
+    ctf = platforms[platform]
+    return ctf(config_filename)
+
+def parse_challenge_name(name):
+    parsed_name = ''
+    for c in name:
+        if c in parsed_challenge_alphabet:
+            parsed_name += c
+        else:
+            parsed_name += '_'
+    return parsed_name
